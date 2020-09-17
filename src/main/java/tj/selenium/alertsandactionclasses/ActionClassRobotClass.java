aaf5cb550048fbcf896a6177b20ffb96e263d543
TharaniJ <tharni89@gmail.com>
2020-09-17 15:39:57 +0530

    Update ActionClassRobotClass.java

--- src/main/java/tj/selenium/alertsandactionclasses/ActionClassRobotClass.java
+++ src/main/java/tj/selenium/alertsandactionclasses/ActionClassRobotClass.java
@@ -2,12 +2,16 @@ package tj.selenium.alertsandactionclasses;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class ActionClassRobotClass {
     public static void main(String[] args) {
 
+        Logger Loggers = LoggerFactory.getLogger(ActionClassRobotClass.class);
         //Setting the driver path
         System.setProperty("webdriver.chrome.driver", "C:/EDU/Software/chromedriver_win32/chromedriver.exe");
         //Instantiating the webdriver
@@ -43,7 +47,23 @@ public class ActionClassRobotClass {
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.keyRelease(KeyEvent.VK_ENTER);
 
-            //robot.delay(1000);
+            robot.delay(1000);
+
+            //tag keys to get to enter and save file
+            for (int i = 1; i <= 3; i++) {
+                robot.keyPress(KeyEvent.VK_TAB);
+                robot.delay(1000);
+                robot.keyRelease(KeyEvent.VK_TAB);
+            }
+
+            // Click the 'Save-As' option by pressing the ENTER key
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
+            robot.delay(1000);
+
+            Loggers.info("Page saved successfully");
+
 
         }catch (Exception e){
             e.printStackTrace();
